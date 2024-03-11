58de74f76051ce49282aea998937f2ed8be2699a
lanmower <almagestfraternite@gmail.com>
2024-03-11 23:18:07 +0200

    art

--- Player.gd
+++ Player.gd
@@ -2,18 +2,21 @@ extends CharacterBody2D
 
 signal health_changed(health_value)
 
-@onready var raycast = $RayCast2D
-
+@onready var raycast = $Node2D/RayCast2D
+@onready var camera = $Camera2D
+@onready var node = $Node2D
 var health = 3
 
 const SPEED = 200.0
-
+var gravity = 900.0
+var JUMP_VELOCITY = -600
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
 
 func _ready():
 	if not is_multiplayer_authority(): return
-
+	camera.make_current()
+	
 func _unhandled_input(event):
 	if not is_multiplayer_authority(): return
 		
@@ -25,12 +28,15 @@ func _unhandled_input(event):
 
 func _physics_process(delta):
 	if not is_multiplayer_authority(): return
-	look_at(get_global_mouse_position())
+	
+
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	velocity.x = input_dir.x * SPEED
-	velocity.y = input_dir.y * SPEED
-
+	#velocity.y = input_dir.y * SPEED
+	velocity.y += gravity * delta
 	move_and_slide()
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 @rpc("any_peer")
 func receive_damage():
